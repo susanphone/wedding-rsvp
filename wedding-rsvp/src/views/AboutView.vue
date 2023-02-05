47b508ab3925bfb3e700f681aed69356d5073dd9
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2023-02-04 19:04:03 -0700

    itinerary

--- wedding-rsvp/src/views/AboutView.vue
+++ wedding-rsvp/src/views/AboutView.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Trip Itinerary</h1>
+    <h2>Monday June 12th</h2>
+    <p>Flight arrives around 10am</p>
+    <p>Train from Paddington to Penzance</p>
+    <p>Check in to Artist Residence at 15:00</p>
+
+    <h2>Tuesday June 13th</h2>
+    <p>Tour St. Michaels Mount</p>
+
+    <h2>Wednesday June 14th</h2>
+    <p>Bride and Groom will be getting ready at 13:00 and going with photographer at 14:00 to take photos.</p> 
+    <p>Ceremony is at 16:00</p>
+    <p>Picture with family and friends with drinks provided by the couple</p>
+    <p>Dinner is following the ceremony (TBD)</p>
+
+    <h2>Thursday June 15th</h2>
+    <p>Check out by 11:00</p>
   </div>
 </template>
 
